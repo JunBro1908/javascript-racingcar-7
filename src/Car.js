4954e0e49a84a9cd673a5a21e08087c8e8d4b734
JunHyeong Kim <junbro1908@gmail.com>
2024-10-25 14:37:09 +0900

    feat(car): Create car object and implement basic logic
    Created a car object with name and current position as attributes and implemented basic logic for car movement (advances when random value is 4 or higher).

---
+++ src/Car.js
@@ -0,0 +1,17 @@
+const { Random } = require("@woowacourse/mission-utils");
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    const randomValue = Random.pickNumberInRange(0, 9);
+    if (randomValue >= 4) {
+      this.position += 1;
+    }
+  }
+}
+
+module.exports = Car;
